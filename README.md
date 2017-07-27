90ade5d5ff89630d646904c277e5658037661a7b
Zuri Pabon <zpabon@itrsgroup.com>
2017-07-28 00:04:27 +0200

    C++ container slides

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 
 Teaching is a great, transforming experience we should all try at least once in our lifetime. Below you may find a list of talks I made sometimes just for myself, for my family or for my workmates.  
 
-
 * The JavaScript Runetime: An Initiation Ceremony ...
 
-  Have you ever heard about Runes ? Do you know what it is ? ... ok, it's like a ancient language and knowledge, pretty hard to understand, to acquire, that let you make wonders. It's like magic ! But we all are serious engineers who do not believe in magic and things like that, right? so I'm gonna talk rather about something much more interesting, the JavaScript Runtime. But before teaching others I wanted to make sure I was aligned with most serious senior JavaScript engineers (who do not believe in magic and things like that) and they all agree in that the JavaScript Runtime is "something like, like ... an ancient knowledge, pretty hard to understand and to acquire. It's like, like magic ... "
+  Have you ever heard about Runes ? Do you know what it is ? ... yeah it's like a ancient language and knowledge, pretty hard to understand, to acquire, that let you make wonders. It's like magic ! But we all are serious engineers who do not believe in magic and things like that, right? so I'm gonna talk rather about something much more interesting, the JavaScript Runtime. But before teaching others I wanted to make sure I was aligned with most serious senior JavaScript engineers (who do not believe in magic and things like that) and they all agree in that the JavaScript Runtime is "something like, like ... an ancient knowledge, pretty hard to understand and to acquire. It's like, like magic ... "
 
   Keep by my side in this magical journey where we will go from the JavaScript RuneTime to the JavaScript Runtime and will unveil its magic !
+
+  Video:
 
   Slides: http://zuri.website/talks/js_runtime/1_js_runetime/
 
@@ -19,4 +20,24 @@ Teaching is a great, transforming experience we should all try at least once in 
 
   Level: Easy
 
-  Length: 45 minutes aprox. (no QA included)
+  Length: 35 minutes aprox. (no QA included)
+
+* Getting Started With Language-Oriented Programming
+
+Data is probably the most valuable and requested asset all over the world. We humans are eager to consume it and are constantly looking for ever-complex, accurate, specific data sets to increase our knowledge stock and keep it up to date. Whether application logs aggregation analysis, trading market rates comparison or simply the weather forecast analysis, general-purpose languages cannot fully provide users with the best tools for interacting, retrieving and solving domain related problems within the context a domain expert would expect to. We will drive a general introduction to this topic during the first part and will get into the job by designing and implementing a nice case example domain specific language for fetching Github issues in the second part.
+
+Video:
+
+Slides: http://zuri.website/talks/language-oriented-programming
+
+Text: https://github.com/Tsur/talks/blob/master/language-oriented-programming/transcript.md
+
+Demos: http://zuri.website/octoql
+
+Resources: https://github.com/Tsur/talks/tree/master/language-oriented-programming
+
+Posts: Part1 · Part2
+
+Level: Easy
+
+Length: 35 minutes aprox. (no QA included)
--- js_runtime/1_js_runetime/examples/shell.cc
+++ js_runtime/1_js_runetime/examples/shell.cc
@@ -1,3 +1,4 @@
+
 #include <include/v8.h>
 #include <include/libplatform/libplatform.h>
 #include <assert.h>
@@ -5,82 +6,192 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
+#include <random>
+
+using namespace v8;
+using namespace std;
 
 /**
- * This sample program shows how to implement a simple javascript container
- * based on v8 that runs a simple REPL.
+ * This sample program shows how to implement a simple javascript shell
+ * based on V8.  This includes initializing V8 with command line options,
+ * creating global functions, compiling and executing strings.
+ *
+ * For a more sophisticated shell, consider using the debug shell D8.
  */
+
+Local<Context> CreateShellContext(Isolate* isolate);
+void RunREPL(Local<Context> context);
+string EvaluateCode(Local<Context> context, string codeToBeEvaluated);
+void Quit(const FunctionCallbackInfo<Value>& args);
+void ReportException(Isolate* isolate, TryCatch* handler);
+string readUserInput();
+void print(string str);
+Isolate* initV8(int argc, char* argv[]);
+
 int main(int argc, char* argv[]) {
-  fprintf(stderr, "V8 version %s [sample shell]\n", v8::V8::GetVersion());
-  v8::Local<v8::Context> context = create_runtime_context();
-  run_repl(context);
+  fprintf(stderr, "My JavaScript Container, a simple repl shell (V8 version %s)\n", V8::GetVersion());
+  /******************************************************/
+  /* Init V8: Location, ExternalData, Platform, Isolate */
+  /******************************************************/
+  Isolate* isolate = initV8(argc, argv); // iframes, tab, web workers
+  /*******************************************************/
+  /* Start V8 Scope                                      */
+  /*******************************************************/
+  // Enter the created isolate scope
+  Isolate::Scope isolate_scope(isolate);
+  HandleScope handle_scope(isolate);
+  // Create a local context and enter it
+  Local<Context> context = CreateShellContext(isolate);
+  Context::Scope context_scope(context);
+  RunREPL(context);
+  /*******************************************************/
+  /* Clean V8: Location, ExternalData, Platform, Isolate */
+  /*******************************************************/
+  isolate->Dispose();
+  V8::Dispose();
+  V8::ShutdownPlatform();
+  // delete platform;
+  // delete create_params.array_buffer_allocator;
   return 0;
 }
 
-/**
- * read -> evaluate -> print -> loop (REPL)
- */
-void run_repl(v8::Local<v8::Context> runtime_context){
-  while(true){
-    char* code_to_be_evaluated = read_user_input();
-    if (code_to_be_evaluated == NULL) break;
-    v8::Local<v8::Value> code_result = evaluate_code(runtime_context, code_to_be_evaluated);
-    if (code_result->IsUndefined()) break;
-    print(code_result);
-  }
+Isolate* initV8(int argc, char* argv[]) {
+  V8::InitializeICUDefaultLocation(argv[0]);
+  V8::InitializeExternalStartupData(argv[0]);
+  V8::InitializePlatform(platform::CreateDefaultPlatform());
+  V8::Initialize();
+  Isolate::CreateParams create_params;
+  create_params.array_buffer_allocator =
+      ArrayBuffer::Allocator::NewDefaultAllocator();
+  return Isolate::New(create_params);
 }
 
-char* read_user_input(){
-  static const int kBufferSize = 256;
-  char buffer[kBufferSize];
-  fprintf(stderr, "> ");
-  char* str = fgets(buffer, kBufferSize, stdin);
-  return str;
+// Extracts a C string from a V8 Utf8Value.
+const char* ToCString(const String::Utf8Value& value) {
+  return *value ? *value : "<string conversion failed>";
 }
 
-v8::Local<v8::Value> result evaluate_code(v8::Local<v8::Context> runtime_context, char* code_to_be_evaluated) {
-  v8::Local<v8::Value> result;
-  v8::Local<v8::Script> script;
-  v8::Script::Compile(runtime_context, code_to_be_evaluated).ToLocal(&script);
-  script->Run(runtime_context).ToLocal(&result);
-  return result;
+// Creates a new execution environment containing the built-in
+// functions.
+Local<Context> CreateShellContext(Isolate* isolate) {
+  // Create a template for the global object.
+  Local<ObjectTemplate> global = ObjectTemplate::New(isolate);
+  // Bind the 'quit' function
+  global->Set(String::NewFromUtf8(
+                  isolate, "quit", NewStringType::kNormal).ToLocalChecked(),
+              FunctionTemplate::New(isolate, Quit));
+  return Context::New(isolate, NULL, global);
 }
-
-void print(v8::Local<v8::Value> result){
-  v8::String::Utf8Value str(result);
-  printf("%s\n", *str);
-}
-
-// Creates a new execution environment containing the built-in functions.
-v8::Local<v8::Context> create_runtime_context() {
-  // Createa global object as the context
-  v8::Local<v8::ObjectTemplate> global = v8::ObjectTemplate::New();
-  global->Set(
-      v8::String::NewFromUtf8(isolate, "escupir", v8::NewStringType::kNormal)
-          .ToLocalChecked(),
-      v8::FunctionTemplate::New(isolate, Escupir));
-  global->Set(
-      v8::String::NewFromUtf8(isolate, "quit", v8::NewStringType::kNormal)
-          .ToLocalChecked(),
-      v8::FunctionTemplate::New(isolate, Quit));
-  return v8::Context::New(global);
-}
-
-
-// The callback that is invoked by v8 whenever the JavaScript 'print'
-// function is called.  Prints its arguments on stdout separated by
-// spaces and ending with a newline.
-void Escupir(const v8::FunctionCallbackInfo<v8::Value>& args) {
-  for (int i = 0; i < args.Length(); i++) {
-    printf("%s", *args[i]);
-  }
-  printf("\n");
-  fflush(stdout);
-}
-
 
 // The callback that is invoked by v8 whenever the JavaScript 'quit'
 // function is called.  Quits.
-void Quit(const v8::FunctionCallbackInfo<v8::Value>& args) {
-  exit(0);
+void Quit(const FunctionCallbackInfo<Value>& args) {
+  // If not arguments are given args[0] will yield undefined which
+  // converts to the integer value 0.
+  int exit_code =
+      args[0]->Int32Value(args.GetIsolate()->GetCurrentContext()).FromMaybe(0);
+  fflush(stdout);
+  fflush(stderr);
+  exit(exit_code);
+}
+
+// The read-eval-execute loop of the shell.
+void RunREPL(Local<Context> context) {
+  // Enter the execution environment before evaluating any code.
+  while (true) {
+    string codeToBeEvaluated = readUserInput();
+    if(codeToBeEvaluated == "\n") continue;
+    string codeEvaluated = EvaluateCode(context, codeToBeEvaluated);
+    print(codeEvaluated);
+  }
+}
+
+void print(string str){
+  printf("%s\n", str.c_str());
+}
+
+string readUserInput() {
+  static const int kBufferSize = 256;
+  char buffer[kBufferSize];
+  fprintf(stderr, "> ");
+  const char* input = fgets(buffer, kBufferSize, stdin);
+  string userInputString(input);
+  return userInputString;
+}
+
+// Executes a string within the current v8 context.
+string EvaluateCode(Local<Context> context, string codeToBeEvaluated) {
+  Local<Script> script;
+  Local<Value> result;
+  Local<String> sourceCode(String::NewFromUtf8(context->GetIsolate(), codeToBeEvaluated.c_str(), NewStringType::kNormal).ToLocalChecked());
+  Local<String> name(String::NewFromUtf8(context->GetIsolate(), "(shell)", NewStringType::kNormal).ToLocalChecked());
+  ScriptOrigin origin(name);
+  TryCatch try_catch(context->GetIsolate());
+  string undefined("");
+
+  if (!Script::Compile(context, sourceCode, &origin).ToLocal(&script)) {
+    // Print errors that happened during compilation.
+    ReportException(context->GetIsolate(), &try_catch);
+    return undefined;
+  }
+
+  if (!script->Run(context).ToLocal(&result)) {
+    assert(try_catch.HasCaught());
+    // Print errors that happened during execution.
+    ReportException(context->GetIsolate(), &try_catch);
+    return undefined;
+  }
+
+  if (!result->IsUndefined()) {
+    // If all went well and the result wasn't undefined then print
+    // the returned value.
+    String::Utf8Value strOf(result);
+    string str(*strOf);
+    return str;
+  }
+
+  return undefined;
+}
+
+void ReportException(Isolate* isolate, TryCatch* try_catch) {
+  HandleScope handle_scope(isolate);
+  String::Utf8Value exception(try_catch->Exception());
+  const char* exception_string = ToCString(exception);
+  Local<Message> message = try_catch->Message();
+  if (message.IsEmpty()) {
+    // V8 didn't provide any extra information about this error; just
+    // print the exception.
+    fprintf(stderr, "%s\n", exception_string);
+  } else {
+    // Print (filename):(line number): (message).
+    String::Utf8Value filename(message->GetScriptOrigin().ResourceName());
+    Local<Context> context(isolate->GetCurrentContext());
+    const char* filename_string = ToCString(filename);
+    int linenum = message->GetLineNumber(context).FromJust();
+    fprintf(stderr, "%s:%i: %s\n", filename_string, linenum, exception_string);
+    // Print line of source code.
+    String::Utf8Value sourceline(
+        message->GetSourceLine(context).ToLocalChecked());
+    const char* sourceline_string = ToCString(sourceline);
+    fprintf(stderr, "%s\n", sourceline_string);
+    // Print wavy underline (GetUnderline is deprecated).
+    int start = message->GetStartColumn(context).FromJust();
+    for (int i = 0; i < start; i++) {
+      fprintf(stderr, " ");
+    }
+    int end = message->GetEndColumn(context).FromJust();
+    for (int i = start; i < end; i++) {
+      fprintf(stderr, "^");
+    }
+    fprintf(stderr, "\n");
+    Local<Value> stack_trace_string;
+    if (try_catch->StackTrace(context).ToLocal(&stack_trace_string) &&
+        stack_trace_string->IsString() &&
+        Local<String>::Cast(stack_trace_string)->Length() > 0) {
+      String::Utf8Value stack_trace(stack_trace_string);
+      const char* stack_trace_string = ToCString(stack_trace);
+      fprintf(stderr, "%s\n", stack_trace_string);
+    }
+  }
 }
